9ceee21315a6c9dc951924916fe802df090c8d03
christopherchoe <chchchoe@gmail.com>
2019-05-17 23:17:09 +0000

    initial commit of api responsive page

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,32 @@
+let amenities = {};
+$(() => {
+
+  $('input').on('click', function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('DIV.amenities H4').text(() => {
+      let arr = Object.values(amenities);
+      if (arr.length === 0) return String.fromCharCode(160);
+      let str = arr.join(', ');
+      return str;
+    });
+  });
+
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    success: function (result, statusCode) {
+      if (statusCode == 200) {
+        $('DIV#api_status').addClass('available');
+      } else {
+        if ($('DIV#api_status').hasClass('available')) {
+          $('DIV#api_status').removeClass('available');
+	}
+      }
+    }
+  });
+});
+
+
